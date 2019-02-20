0f8a63c5c1ec69f834c91554192224d579eee8c8
France Gelasque <france.workmail@gmail.com>
2019-02-20 18:56:29 +0800

    Controls: Notification fixes on album art cover and play/pause button

--- app/src/main/java/france/apps/musify/utils/NotificationService.java
+++ app/src/main/java/france/apps/musify/utils/NotificationService.java
@@ -1,13 +1,18 @@
 package france.apps.musify.utils;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
@@ -119,25 +124,86 @@ public class NotificationService extends Service {
         views.setTextViewText(R.id.status_bar_artist_name, "Artist Name");
         bigViews.setTextViewText(R.id.status_bar_artist_name, "Artist Name");
         bigViews.setTextViewText(R.id.status_bar_album_name, "Album Name");
-        status = new Notification.Builder(this).build();
-        status.contentView = views;
-        status.bigContentView = bigViews;
-        status.flags = Notification.FLAG_ONGOING_EVENT;
-        status.icon = R.mipmap.ic_headphones;
-        status.contentIntent = pendingIntent;
 
 
 
-        if(notificationTarget==null)
-            notificationTarget = new NotificationTarget(
+
+//        status = new Notification.Builder(this).build();
+//        status.contentView = views;
+//        status.bigContentView = bigViews;
+//        status.flags = Notification.FLAG_ONGOING_EVENT;
+//        status.icon = R.mipmap.ic_headphones;
+//        status.contentIntent = pendingIntent;
+
+
+
+//        if(notificationTarget==null)
+//            notificationTarget = new NotificationTarget(
+//                    this,
+//                    R.id.status_bar_album_art,
+//                    views,
+//                    status,
+//                    Constants.NOTIFICATION_ID.FOREGROUND_SERVICE);
+
+
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_headphones)
+                        .setContentTitle("Content Title")
+                        .setContentText("Content Text")
+                        .setContent(views)
+                        .setCustomBigContentView(bigViews)
+                        .setPriority( NotificationCompat.PRIORITY_DEFAULT);
+
+        status  = mBuilder.build();
+
+        NotificationManager mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(Constants.NOTIFICATION_ID.FOREGROUND_SERVICE, status);
+
+
+        if(notificationTargetAlbumArtSmall==null)
+            notificationTargetAlbumArtSmall = new NotificationTarget(
+                this,
+                R.id.status_bar_album_art,
+                views,
+                status,
+                Constants.NOTIFICATION_ID.FOREGROUND_SERVICE);
+        if(notificationTargetAlbumArtExpanded==null)
+            notificationTargetAlbumArtExpanded = new NotificationTarget(
+                this,
+                R.id.status_bar_album_art,
+                bigViews,
+                status,
+                Constants.NOTIFICATION_ID.FOREGROUND_SERVICE);
+
+
+        if(notificationTargetPlayPauseSmall==null)
+            notificationTargetPlayPauseSmall = new NotificationTarget(
+                this,
+                R.id.status_bar_play,
+                views,
+                status,
+                Constants.NOTIFICATION_ID.FOREGROUND_SERVICE);
+
+        if(notificationTargetPlayPauseExpanded==null)
+            notificationTargetPlayPauseExpanded = new NotificationTarget(
                     this,
-                    R.id.status_bar_album_art,
-                    views,
+                    R.id.status_bar_play,
+                    bigViews,
                     status,
                     Constants.NOTIFICATION_ID.FOREGROUND_SERVICE);
 
-        if(listener!=null)
+
+
+
+
+        if(listener!=null) {
             listener.OnNewTrackOpened(MusifyPlayer.getCurrentlyPlayedMusic());
+
+            if(MusifyPlayer.isPlaying())
+                listener.OnPlay(MusifyPlayer.getCurrentlyPlayedMusic());
+            else listener.OnPause(MusifyPlayer.getCurrentlyPlayedMusic());
+        }
         MusifyPlayer.addListener(listener);
 
 
@@ -147,28 +213,55 @@ public class NotificationService extends Service {
 
     }
 
-    private NotificationTarget notificationTarget;
+    private NotificationTarget notificationTargetAlbumArtSmall;
+    private NotificationTarget notificationTargetAlbumArtExpanded;
+    private NotificationTarget notificationTargetPlayPauseSmall;
+    private NotificationTarget notificationTargetPlayPauseExpanded;
+
+
     private MusifyPlayer.OnPlayerChangesListener listener = new MusifyPlayer.OnPlayerChangesListener() {
         @Override
         public void OnPause(PlayableMedia item) {
 
 
+            try {
+                Glide
+                        .with(NotificationService.this)
+                        .asBitmap()
+                        .load(R.mipmap.ic_play_arrow)
+                        .into(notificationTargetPlayPauseSmall);
 
 
-            views.setImageViewResource(R.id.status_bar_play,
-                    R.mipmap.ic_play_arrow);
-            bigViews.setImageViewResource(R.id.status_bar_play,
-                    R.mipmap.ic_play_arrow);
+                Glide
+                        .with(NotificationService.this)
+                        .asBitmap()
+                        .load(R.mipmap.ic_play_arrow)
+                        .into(notificationTargetPlayPauseExpanded);
+            } catch (IllegalStateException e) {//IllegalStateException: Can't parcel a recycled bitmap
+                e.printStackTrace();
+            }
         }
 
         @Override
         public void OnPlay(PlayableMedia item) {
 
 
-            views.setImageViewResource(R.id.status_bar_play,
-                    R.mipmap.ic_pause);
-            bigViews.setImageViewResource(R.id.status_bar_play,
-                    R.mipmap.ic_pause);
+            try {
+                Glide
+                        .with(NotificationService.this)
+                        .asBitmap()
+                        .load(R.mipmap.ic_pause)
+                        .into(notificationTargetPlayPauseSmall);
+
+
+                Glide
+                        .with(NotificationService.this)
+                        .asBitmap()
+                        .load(R.mipmap.ic_pause)
+                        .into(notificationTargetPlayPauseExpanded);
+            } catch (IllegalStateException e) {//IllegalStateException: Can't parcel a recycled bitmap
+                e.printStackTrace();
+            }
 
 
         }
@@ -188,15 +281,32 @@ public class NotificationService extends Service {
             bigViews.setTextViewText(R.id.status_bar_artist_name, artist);
 
 
+            try {
+                Glide
+                    .with(NotificationService.this)
+                    .asBitmap()
+                    .load(item.getAudio_image())
+                    .into(notificationTargetAlbumArtSmall);
 
 
+                Glide
+                        .with(NotificationService.this)
+                        .asBitmap()
+                        .load(item.getAudio_image())
+                        .into(notificationTargetAlbumArtExpanded);
+            } catch (IllegalStateException e) {//IllegalStateException: Can't parcel a recycled bitmap
+                e.printStackTrace();
+            }
 
 
-            Glide
-                .with(NotificationService.this)
-                .asBitmap()
-                .load(item.getAudio_image())
-                .into(notificationTarget);
+            if(URLUtil.isValidUrl(item.getAudio_image())){
+                views.setViewVisibility(R.id.status_bar_icon, View.GONE);
+                views.setViewVisibility(R.id.status_bar_album_art, View.VISIBLE);
+            }
+            else{
+                views.setViewVisibility(R.id.status_bar_icon, View.VISIBLE);
+                views.setViewVisibility(R.id.status_bar_album_art, View.GONE);
+            }
         }
 
         @Override
